de34d4ab4b130d435575211114f910128f29edba
Ekansh Verma <21bee083@nith.ac.in>
2023-08-04 14:08:34 +0530

    firebase integrated for my list

--- netflix-api/server.js
+++ netflix-api/server.js
@@ -1,27 +1,27 @@
-const express = require("express");
-const cors = require("cors");
-const userRoutes = require("./routes/UserRoutes");
-const mongoose = require("mongoose");
+// const express = require("express");
+// const cors = require("cors");
+// const userRoutes = require("./routes/UserRoutes");
+// const mongoose = require("mongoose");
 
-const app = express();
+// const app = express();
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
-mongoose
-  .connect("mongodb://localhost:27017/netflix", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("DB Connetion Successfull");
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+// mongoose
+//   .connect("mongodb://localhost:27017/netflix", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("DB Connetion Successfull");
+//   })
+//   .catch((err) => {
+//     console.log(err.message);
+//   });
 
-app.use("/api/user", userRoutes);
+// app.use("/api/user", userRoutes);
 
-app.listen(5000, () => {
-  console.log("server started on port 5000");
-});
+// app.listen(5000, () => {
+//   console.log("server started on port 5000");
+// });
